13a7694382f0cc24f04ea9e7878d9b66221e3f8c
Abdelrhman-youseff <20223187@fue.edu.eg>
2024-09-02 14:57:24 +0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,32 @@
+// let test: number = 1;
+// console.log("here");
+// let bar: string |  number = 3;
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    
+}
+
+interface Store {
+    products: Product[];
+    addProduct: (product: Product) => void;
+    
+}
+
+class ProductStore implements Store {
+    products: Product[] = [];
+
+    addProduct(product: Product) {
+        this.products.push(product);
+    }   
+}
+
+const store = new ProductStore();
+
+const product1: Product = { id: 1, name: 'Mouse', price: 999.99};
+const product2: Product = { id: 2, name: 'Keyboard', price: 19.99};
+
+store.addProduct(product1);
+store.addProduct(product2);
+
